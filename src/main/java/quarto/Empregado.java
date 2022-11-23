31c809f9736327316ec07842dc7a37e98f26d3f3
vilarinho23 <davidvilarinho@ua.pt>
2022-11-23 20:59:55 +0000

    Add Package Quarto
    Add Package Quarto: Gestor de Quartos; Empregado
    
    Signed-off-by: vilarinho23 <davidvilarinho@ua.pt>

---
+++ src/main/java/quarto/Empregado.java
@@ -0,0 +1,14 @@
+package quarto;
+
+public class Empregado {
+    private int empregadoId;
+    private String cargo;
+    private float salario;
+    private String horaEntrada;
+    private String horaSaida;
+    private String morada;
+    private int nif;
+    private int telefone;
+
+    public Empregado(){}
+}
---
+++ src/main/java/quarto/GestorDeQuartos.java
@@ -0,0 +1,15 @@
+package quarto;
+
+import java.util.ArrayList;
+
+public class GestorDeQuartos {
+    private ArrayList<Empregado> empregados;
+
+    public GestorDeQuartos(){   }
+
+    protected Empregado procurarEmpregadoPorNIF(){return null;}
+
+    protected Empregado procurarEmpregadoPorID(){return null;}
+
+    protected boolean adicionarEmpregado(){return false;}
+}
