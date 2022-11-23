14ce72a25e9b377ebc0e2afdba9c72bb6b034b2f
vilarinho23 <davidvilarinho@ua.pt>
2022-11-23 22:24:59 +0000

    Add Package Cliente
    Add Package Cliente: GestorDeClientes; Cliente
    
    Signed-off-by: vilarinho23 <davidvilarinho@ua.pt>

---
+++ src/main/java/cliente/Cliente.java
@@ -0,0 +1,9 @@
+package cliente;
+
+public class Cliente {
+    private int nif;
+    private String nome;
+    private int telefone;
+
+    public Cliente(){}
+}
---
+++ src/main/java/cliente/GestorDeClientes.java
@@ -0,0 +1,13 @@
+package cliente;
+
+import java.util.HashMap;
+
+public class GestorDeClientes {
+    private HashMap<Integer, Cliente> clientes;
+
+    public GestorDeClientes(){}
+
+    protected Cliente procurarClientePorNIF(){return null;}
+
+    protected boolean adicionarCliente(){return false;}
+}
