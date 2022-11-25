eca65c32db9f0c107c8c269e63d6ca97a40adbcf
João Rodrigues <jpprodrigues@ua.pt>
2022-11-25 22:25:12 +0000

    Add Package Empregado
    Add Package Empregado: Gestor de Empregados; Empregado

---
+++ src/main/java/empregado/GestorDeEmpregados.java
@@ -0,0 +1,23 @@
+package empregado;
+
+import java.util.HashMap;
+
+public class GestorDeEmpregados {
+
+    private HashMap <Integer,Empregado> empregados;
+
+    public GestorDeEmpregados(){}
+
+    protected Empregado procurarEmpregadoPorNIF(){
+        return null;
+    }
+
+    protected Empregado procurarEmpregadoPorID(){
+        return null;
+    }
+
+    protected boolean adicionarEmpregado(){
+        return false;
+    }
+
+}
