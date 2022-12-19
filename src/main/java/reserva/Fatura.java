54ce1fcca8c302efffe502dad536e80ce4943467
João <joaop.oliveira@ua.pt>
2022-12-19 23:10:16 +0000

    Adicionada funcção para procurar reservas com base no nif do cliente
    Recebe um NIF válido, procura na base de dados por todas as entradas com valor igual. A seguir, o método retorna uma lista com as reservas encontradas, se houver.
    
    Por cada reserva encontrada, procurar se existe fatura correspondente. Se existe guardar dados na respetiva reserva.
    
    Removido lista no 'GestorDeReserva', com o propósito de armazenar em cache as reservas. O método procurar reservas apenas com o mesmo nif do cliente, logo só ao procurar o cache não se saberia se foram encontradas todas as reservas desse cliente.
    
    Signed-off-by: João <joaop.oliveira@ua.pt>

--- src/main/java/reserva/Fatura.java
+++ src/main/java/reserva/Fatura.java
@@ -1,9 +1,16 @@
 package reserva;
 
 public class Fatura {
-    private int faturaId;
-    private float montante;
-    private boolean estado;
+    private final int faturaId;
+    private final float montanteFinal;
 
-    public Fatura(){};
+    public Fatura(int id, float montanteFinal){
+        this.faturaId = id;
+        this.montanteFinal = montanteFinal;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Fatura #%d | Montante pago: %.2f€", faturaId, montanteFinal);
+    }
 }
--- src/main/java/reserva/Reserva.java
+++ src/main/java/reserva/Reserva.java
@@ -4,11 +4,35 @@ import java.util.ArrayList;
 
 public class Reserva {
 
-    private int reservaId;
-    private boolean reservaPaga;
+    private final int reservaId;
+    private final boolean reservaPaga;
     private Fatura fatura;
     private ArrayList<Integer> quartos;
-    private int clientNIF;
+    private final int clienteNIF;
+    private final int empregadoNIF;
+
+    public Reserva(int reservaId, int clienteNIF, int empregadoId, boolean estadoPagamento, Fatura fatura){
+        this.reservaId = reservaId;
+        this.clienteNIF = clienteNIF;
+        this.empregadoNIF = empregadoId;
+        this.reservaPaga = estadoPagamento;
+        this.fatura = fatura;
+    }
+
+    public int getClienteNIF(){ return clienteNIF; }
 
     protected Fatura gerarFatura(){ return null; }
+
+
+    @Override
+    public String toString() {
+        String estadoFatura;
+        if(reservaPaga){
+            if(fatura == null) estadoFatura = "Reserva paga. Informação da fatura indisponível.";
+            else estadoFatura = fatura.toString();
+        }else estadoFatura = "Pagamento não efetuado.";
+
+        return String.format("Reseva de Quarto #%d | NIF do Cliente: %d | Empregado ID: %d | %s",
+                reservaId, clienteNIF, empregadoNIF,estadoFatura);
+    }
 }
