40621f65b05fd66ef9196c54ba8de14f4482e5ef
vilarinho23 <davidvilarinho@ua.pt>
2022-12-20 23:24:01 +0000

    Add Cliente Search
    Com o método procurarClientePorNif, podemos procurar um cliente através do nif, primeiro no cache e caso não exista, na base de dados.

--- src/main/java/cliente/GestorDeClientes.java
+++ src/main/java/cliente/GestorDeClientes.java
@@ -1,11 +1,33 @@
 package cliente;
 
+import basededados.GestorDeBaseDeDados;
+
 import java.util.HashMap;
+import java.util.List;
 
 public class GestorDeClientes {
     private HashMap<Integer, Cliente> clientes;
 
-    public GestorDeClientes(){}
+    public GestorDeClientes(){
+        this.clientes = new HashMap<Integer, Cliente>();
+    }
+
+    protected Cliente procurarClientePorNIF(int nif, GestorDeBaseDeDados gestorBD){
+        if(clientes.containsKey(nif)){
+            return clientes.get(nif);
+        }else{
+            String query = String.format("SELECT * FROM Clientes WHERE Nif=%d", nif);
+            List<String> dadosCliente = gestorBD.tryQueryDatabase(query);
+            if(dadosCliente.isEmpty()){
+                return null;
+            }else{
+                String[] dados = dadosCliente.get(0).split(",");
+                Cliente cliente = new Cliente(Integer.parseInt(dados[0]), dados[1], Integer.parseInt(dados[2]));
+                adicionarClienteCache(cliente);
+                return cliente;
+            }
+        }
+    }
 
     protected boolean adicionarCliente(int nif, String nome, int telefone, GestorDeBaseDeDados gestorBD){
         String query = String.format("REPLACE INTO Clientes VALUES ('%d', '%s', '%d')", nif, nome, telefone);
