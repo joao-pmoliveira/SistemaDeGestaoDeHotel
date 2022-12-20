205bd2d795b96e3f6832a1fda8fc2080787d78b4
vilarinho23 <davidvilarinho@ua.pt>
2022-12-20 23:09:18 +0000

    Add Cliente Insert Methods
    Com o método adicionarCliente, podemos adicionar um cliente novo à base de dados.
    Com o método adicionarClienteCache podemos adicionar um cliente novo à cache.

--- src/main/java/cliente/GestorDeClientes.java
+++ src/main/java/cliente/GestorDeClientes.java
@@ -7,7 +7,28 @@ public class GestorDeClientes {
 
     public GestorDeClientes(){}
 
-    protected Cliente procurarClientePorNIF(){return null;}
+    protected boolean adicionarCliente(int nif, String nome, int telefone, GestorDeBaseDeDados gestorBD){
+        String query = String.format("REPLACE INTO Clientes VALUES ('%d', '%s', '%d')", nif, nome, telefone);
 
-    protected boolean adicionarCliente(){return false;}
+        try {
+            gestorBD.tryUpdateDatabase(query);
+            adicionarClienteCache(nif, nome, telefone);
+            return true;
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void adicionarClienteCache(int nif, String nome, int telefone){
+        Cliente cliente = new Cliente(nif, nome, telefone);
+        clientes.put(nif, cliente);
+    }
+
+    private void adicionarClienteCache(Cliente cliente){
+        if(cliente == null){
+            return;
+        }else {
+            clientes.put(cliente.getNif(), cliente);
+        }
+    }
 }
