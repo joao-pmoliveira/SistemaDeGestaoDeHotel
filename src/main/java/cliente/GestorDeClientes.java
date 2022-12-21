c960591fabaa32031ddb8120b0f9ff2627a64d36
David Vilarinho <94756217+vilarinho23@users.noreply.github.com>
2022-12-21 14:52:37 +0000

    Fix Class according to change requests
    Signed-off-by: David Vilarinho <94756217+vilarinho23@users.noreply.github.com>

--- src/main/java/cliente/GestorDeClientes.java
+++ src/main/java/cliente/GestorDeClientes.java
@@ -1,11 +1,29 @@
 package cliente;
 
+import basededados.GestorDeBaseDeDados;
 import java.util.HashMap;
+import java.util.List;
 
 public class GestorDeClientes {
     private HashMap<Integer, Cliente> clientes;
 
-    public GestorDeClientes(){}
+    public GestorDeClientes(){
+        this.clientes = new HashMap<Integer, Cliente>();
+    }
+
+    protected Cliente procurarClientePorNIF(int nif, GestorDeBaseDeDados gestorBD){
+        if( clientes.containsKey(nif) ) return clientes.get(nif);
+
+        String query = String.format( "SELECT * FROM cliente WHERE nif =  %d", nif);
+        List<String> dadosCliente = gestorBD.tryQueryDatabase(query);
+
+        If( dadosCliente.isEmpty() ) return null;
+
+        String [] dados = dadosCliente.get(0).split(",");
+        Cliente cliente = new Cliente(Integer.parseInt(dados[0]), dados[1], Integer.parseInt(dados[2]));
+        adicionarClienteCache(cliente);
+        return cliente;
+    }
 
     protected boolean adicionarCliente(int nif, String nome, int telefone, GestorDeBaseDeDados gestorBD){
         String query = String.format("REPLACE INTO cliente VALUES ('%d', '%s', '%d')", nif, nome, telefone);
