357081dd533a420e3d2560b8914287a0f71b11a8
Joao Oliveira <joaop.oliveira@ua.pt>
2022-12-23 11:52:02 +0000

    Adicionar getters à classe 'Reserva'
    Signed-off-by: Joao Oliveira <joaop.oliveira@ua.pt>

--- src/main/java/reserva/Reserva.java
+++ src/main/java/reserva/Reserva.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class Reserva {
 
-    private final int reservaId;
+    private final int reservaID;
     private final boolean reservaPaga;
     private Fatura fatura;
     private ArrayList<Integer> quartos;
     private final int clienteNIF;
     private final int empregadoNIF;
 
-    public Reserva(int reservaId, int clienteNIF, int empregadoId, boolean estadoPagamento, Fatura fatura){
-        this.reservaId = reservaId;
+    public Reserva(int reservaID, int clienteNIF, int empregadoId, boolean estadoPagamento, Fatura fatura){
+        this.reservaID = reservaID;
         this.clienteNIF = clienteNIF;
         this.empregadoNIF = empregadoId;
         this.reservaPaga = estadoPagamento;
@@ -20,6 +20,10 @@ public class Reserva {
     }
 
     public int getClienteNIF(){ return clienteNIF; }
+    public int getReservaID(){ return reservaID; }
+    public int getEmpregadoNIF(){ return empregadoNIF; }
+    public boolean getEstadoPagamento(){ return reservaPaga; }
+    public Fatura getFatura(){ return fatura; }
 
     protected Fatura gerarFatura(){ return null; }
 
@@ -33,6 +37,6 @@ public class Reserva {
         }else estadoFatura = "Pagamento não efetuado.";
 
         return String.format("Reseva de Quarto #%d | NIF do Cliente: %d | Empregado ID: %d | %s",
-                reservaId, clienteNIF, empregadoNIF,estadoFatura);
+                reservaID, clienteNIF, empregadoNIF,estadoFatura);
     }
 }
