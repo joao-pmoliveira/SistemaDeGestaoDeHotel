223a7c04f18533357e37182268d2a7d51b5e285a
Joao Oliveira <joaop.oliveira@ua.pt>
2022-12-23 15:56:39 +0000

    Adicionar setters e getters à classe
    Signed-off-by: Joao Oliveira <joaop.oliveira@ua.pt>

--- src/main/java/reserva/Reserva.java
+++ src/main/java/reserva/Reserva.java
@@ -4,26 +4,49 @@ import java.util.ArrayList;
 
 public class Reserva {
 
+
     private final int reservaID;
-    private final boolean reservaPaga;
-    private Fatura fatura;
-    private ArrayList<Integer> quartos;
     private final int clienteNIF;
     private final int empregadoNIF;
+    private boolean reservaPaga;
+    private float precoAtual;
+    private Fatura fatura;
+    private ArrayList<Integer> quartos;
 
-    public Reserva(int reservaID, int clienteNIF, int empregadoId, boolean estadoPagamento, Fatura fatura){
+
+    public Reserva(int reservaID, int clienteNIF, int empregadoId, float precoAtual, boolean reservaPaga,Fatura fatura){
         this.reservaID = reservaID;
         this.clienteNIF = clienteNIF;
         this.empregadoNIF = empregadoId;
-        this.reservaPaga = estadoPagamento;
+        this.precoAtual = precoAtual;
+        this.reservaPaga = reservaPaga;
         this.fatura = fatura;
+        quartos = new ArrayList<>();
     }
 
     public int getClienteNIF(){ return clienteNIF; }
     public int getReservaID(){ return reservaID; }
     public int getEmpregadoNIF(){ return empregadoNIF; }
     public boolean getEstadoPagamento(){ return reservaPaga; }
+    public float getPrecoAtual(){ return precoAtual; }
     public Fatura getFatura(){ return fatura; }
+
+    public void setReservaPaga(boolean estado){
+        this.reservaPaga = estado;
+    }
+    public void setFatura(Fatura fatura){
+        if(fatura == null) return;
+        this.fatura = fatura;
+    }
+    public void somarAoPrecoAtual(float precoAdicional){
+        if(precoAdicional <= 0) return;
+        precoAtual += precoAdicional;
+    }
+
+    public void adicionarQuarto(int quartoID){
+        if (quartos.contains(quartoID)) return;
+        quartos.add(quartoID);
+    }
 
     protected Fatura gerarFatura(){ return null; }
 
