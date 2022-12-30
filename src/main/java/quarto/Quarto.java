71e1fbbac9c310b58e2ae3765082263cd2302a0f
vilarinho23 <davidvilarinho@ua.pt>
2022-12-30 19:47:19 +0000

    Adicionada classe Quarto, para inicializar um novo Quarto.

--- src/main/java/quarto/Quarto.java
+++ src/main/java/quarto/Quarto.java
@@ -2,11 +2,22 @@ package quarto;
 
 public class Quarto {
     private int quartoId;
-    private String layoutTipo;
     private int layoutId;
     private float precoBase;
+    private String layoutNome;
+    private String layoutDescricao;
 
-    public Quarto(){}
+    public Quarto(int quartoId, int layoutId, float precoBase, String layoutNome, String layoutDescricao){
+        this.quartoId = quartoId;
+        this.layoutId = layoutId;
+        this.precoBase = precoBase;
+        this.layoutNome = layoutNome;
+        this.layoutDescricao = layoutDescricao;
+    }
 
-    protected boolean getDisponibilidadeEmPeriodo(){return false;}
+    public int getQuartoId(){return quartoId;}
+    public int getLayoutId(){return layoutId;}
+    public float getPrecoBase(){return precoBase;}
+    public String getLayoutNome(){return layoutNome;}
+    public String getLayoutDescricao(){return layoutDescricao;}
 }
