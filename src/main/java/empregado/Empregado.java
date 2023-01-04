2d42d3bec0f07fc7786c0134c97ce69387624eb1
João Rodrigues <jpprodrigues@ua.pt>
2023-01-04 18:00:40 +0000

    Adicinionar funcionalidade para procurar empregado e para adicionar
    Adicionado métodos para procurar empregado por Nif e por ID do mesmo.
    Adicionado método para criação de um novo empregado com o nome, cargo, morada, telefone, nif, salário, Hora de entrada, Hora de saída, e a sua palavra-passe.

--- src/main/java/empregado/Empregado.java
+++ src/main/java/empregado/Empregado.java
@@ -3,15 +3,40 @@ package empregado;
 public class Empregado {
 
     private int id;
-    private int nif;
+    private String nome;
+    private int cargo;
+    private String morada;
     private int telefone;
-    private String cargo;
+    private int nif;
+    private float salario;
     private String horaEntrada;
     private String horaSaida;
-    private String morada;
-    private float salario;
 
-    public Empregado(){}
+
+
+    public Empregado(int id, String nome, int cargo, String morada, int telefone, int nif, float salario, String horaEntrada, String horaSaida){
+        this.id = id;
+        this.nome = nome;
+        this.nif = nif;
+        this.telefone = telefone;
+        this.cargo = cargo;
+        this.horaEntrada = horaEntrada;
+        this.horaSaida = horaSaida;
+        this.morada = morada;
+        this.salario = salario;
+
+    }
+
+    public int getId (){return id;}
+    public String getNome(){return nome;}
+    public int getNif(){return nif;}
+    public int getTelefone(){return telefone;}
+    public int getCargo(){return cargo;}
+    public String getHoraEntrada(){return horaEntrada;}
+    public String getHoraSaida(){return horaSaida;}
+    public String getMorada(){return morada;}
+    public float getSalario(){return salario;}
+
 
 
 }
