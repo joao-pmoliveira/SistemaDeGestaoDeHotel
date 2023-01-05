ed28e459a5f921b2c9903f21b71eca8642c52811
vilarinho23 <davidvilarinho@ua.pt>
2023-01-05 17:37:17 +0000

    Remoção de cache do programa
    Removida cache do programa, nas packages clientes, empregados e quartos.

--- src/main/java/cliente/GestorDeClientes.java
+++ src/main/java/cliente/GestorDeClientes.java
@@ -1,19 +1,14 @@
 package cliente;
 
 import basededados.GestorDeBaseDeDados;
-import java.util.HashMap;
 import java.util.List;
 
 public class GestorDeClientes {
-    private HashMap<Integer, Cliente> clientes;
 
     public GestorDeClientes(){
-        this.clientes = new HashMap<Integer, Cliente>();
     }
 
     public Cliente procurarClientePorNIF(int nif, GestorDeBaseDeDados gestorBD){
-        if( clientes.containsKey(nif) ) return clientes.get(nif);
-
         String query = String.format( "SELECT * FROM cliente WHERE nif =  %d", nif);
         List<String> dadosCliente = gestorBD.tryQueryDatabase(query);
 
@@ -21,7 +16,6 @@ public class GestorDeClientes {
 
         String [] dados = dadosCliente.get(0).split(",");
         Cliente cliente = new Cliente(Integer.parseInt(dados[0]), dados[1], Integer.parseInt(dados[2]));
-        adicionarClienteCache(cliente);
         return cliente;
     }
 
@@ -30,20 +24,9 @@ public class GestorDeClientes {
 
         try {
             gestorBD.tryUpdateDatabase(query);
-            adicionarClienteCache(nif, nome, telefone);
             return true;
         }catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private void adicionarClienteCache(int nif, String nome, int telefone){
-        Cliente cliente = new Cliente(nif, nome, telefone);
-        clientes.put(nif, cliente);
-    }
-
-    private void adicionarClienteCache(Cliente cliente){
-        if(cliente == null)return;
-        clientes.put(cliente.getNIF(), cliente);
     }
 }
