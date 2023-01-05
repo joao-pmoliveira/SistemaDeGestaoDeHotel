d710873d15b254ab79bdcdecae4e1c43f336ef22
Diogo <diogofilipe18@ua.pt>
2023-01-05 15:25:17 +0000

    Inserir e Procurar Neste commit mostro o código que serve para procurar na base de dados e inserir nela também.

--- src/main/java/limpeza/GestorDeLimpeza.java
+++ src/main/java/limpeza/GestorDeLimpeza.java
@@ -1,18 +1,69 @@
 package limpeza;
 
-import java.util.ArrayList;
+
+import basededados.GestorDeBaseDeDados;
+
+
 import java.util.HashMap;
+import java.util.List;
 
 public class GestorDeLimpeza {
     private HashMap<Integer, RegistoDeLimpeza> registos;
 
-    public GestorDeLimpeza(){}
+    public GestorDeLimpeza(){
+        this.registos = new HashMap<Integer, RegistoDeLimpeza>();
+    }
 
-    protected ArrayList<RegistoDeLimpeza> procurarRegistosPorQuarto(){ return null;}
+    public RegistoDeLimpeza procurarRegistosPorQuarto(int numeroQuarto, GestorDeBaseDeDados gestorBD){
 
-    protected ArrayList<RegistoDeLimpeza> procurarRegistosPorEmpregadoId(){ return null;}
 
-    protected ArrayList<RegistoDeLimpeza> procurarRegistosPorData(){ return null;}
+        String query= String.format("SELECT * FROM registo_limpeza WHERE quarto_id = %d ",numeroQuarto );
+        List<String> registoslimpeza = gestorBD.tryQueryDatabase(query);
 
-    protected boolean adicionarRegisto(){ return false;}
+        if( registoslimpeza.isEmpty() ) return null;
+        String [] dados = registoslimpeza.get(0).split(",");
+        RegistoDeLimpeza valores = new RegistoDeLimpeza((dados[0]), Integer.parseInt(dados[1]), Integer.parseInt(dados[2]));
+        return valores;
+
+    }
+
+    public RegistoDeLimpeza procurarRegistosPorEmpregadoId(GestorDeBaseDeDados gestorBD, int idEmpregado){
+
+
+
+        String query= String.format("SELECT * FROM registo_limpeza WHERE empregado_id = %d ",idEmpregado );
+        List<String> registoslimpeza = gestorBD.tryQueryDatabase(query);
+
+        if( registoslimpeza.isEmpty() ) return null;
+        String [] dados = registoslimpeza.get(0).split(",");
+        RegistoDeLimpeza valores = new RegistoDeLimpeza((dados[0]), Integer.parseInt(dados[1]), Integer.parseInt(dados[2]));
+        return valores;
+
+    }
+
+    protected RegistoDeLimpeza procurarRegistosPorData(GestorDeBaseDeDados gestorBD, String data){
+
+
+        String query= String.format("SELECT * FROM registo_limpeza WHERE data_hora = %d ",data );
+        List<String> registoslimpeza = gestorBD.tryQueryDatabase(query);
+
+        if( registoslimpeza.isEmpty() ) return null;
+        String [] dados = registoslimpeza.get(0).split(",");
+        RegistoDeLimpeza valores = new RegistoDeLimpeza((dados[0]), Integer.parseInt(dados[1]), Integer.parseInt(dados[2]));
+        return valores;
+
+        }
+
+    protected boolean adicionarRegisto(String data, int numeroQuarto, int idEmpregado, GestorDeBaseDeDados gestorBD){
+        String query = String.format
+                ("INSERT INTO registo_limpeza (`data_hora`, `quarto_id`, `empregado_id`)  VALUES ('%d', '%s', '%d')", data, numeroQuarto, idEmpregado);
+
+        try {
+            gestorBD.tryUpdateDatabase(query);
+            return true;
+        }catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
--- src/main/java/limpeza/RegistoDeLimpeza.java
+++ src/main/java/limpeza/RegistoDeLimpeza.java
@@ -5,6 +5,15 @@ public class RegistoDeLimpeza {
     private int numeroQuarto;
     private int idEmpregado;
 
-    public RegistoDeLimpeza(){}
+    public RegistoDeLimpeza( String data, int numeroQuarto, int idEmpregado){
+        this.data=data;
+        this.numeroQuarto=numeroQuarto;
+        this.idEmpregado=idEmpregado;
+    }
 
+    public String getData(){return data;}
+
+    public int getNumeroQuarto(){return numeroQuarto;}
+
+    public int getIdEmpregado(){return idEmpregado;}
 }
