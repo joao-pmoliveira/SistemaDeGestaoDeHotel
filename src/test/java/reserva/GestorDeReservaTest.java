91de1f6556204fcf4e4eb2436068649d8d93b0f2
Joao Oliveira <joaop.oliveira@ua.pt>
2023-01-08 16:26:24 +0000

    Inicializa classe de Testes para o Gestor de Reserva
    Cria a classe para os testes de software correspondentes à class GestorDeReserva.
    Inclui também a declaração dos métodos que irão correr antes e depois do conjunto de testes.
    
    Signed-off-by: Joao Oliveira <joaop.oliveira@ua.pt>

---
+++ src/test/java/reserva/GestorDeReservaTest.java
@@ -0,0 +1,18 @@
+package reserva;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GestorDeReservaTest {
+
+    @BeforeAll
+    void setUp() {
+    }
+
+    @AfterAll
+    void tearDown() {
+    }
+
+}
