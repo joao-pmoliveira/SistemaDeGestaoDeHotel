e9e216f972597b73e847415a6f0bb2a9fb8d4462
vilarinho23 <davidvilarinho@ua.pt>
2023-01-17 16:11:00 +0000

    Implementação testes à classe cliente

--- src/main/java/cliente/GestorDeClientes.java
+++ src/main/java/cliente/GestorDeClientes.java
@@ -1,6 +1,8 @@
 package cliente;
 
 import basededados.GestorDeBaseDeDados;
+
+import java.security.InvalidParameterException;
 import java.util.List;
 
 public class GestorDeClientes {
@@ -9,10 +11,11 @@ public class GestorDeClientes {
     }
 
     public Cliente procurarClientePorNIF(int nif, GestorDeBaseDeDados gestorBD){
+        if(gestorBD == null) throw new InvalidParameterException("Gestor de Base de Dados nulo.");
         String query = String.format( "SELECT * FROM cliente WHERE nif =  %d", nif);
         List<String> dadosCliente = gestorBD.tryQueryDatabase(query);
 
-        if( dadosCliente.isEmpty() ) return null;
+        if( dadosCliente.isEmpty() ) throw new InvalidParameterException("Não existe cliente associado ao NIF fornecido");
 
         String [] dados = dadosCliente.get(0).split(",");
         Cliente cliente = new Cliente(Integer.parseInt(dados[0]), dados[1], Integer.parseInt(dados[2]));
@@ -20,13 +23,12 @@ public class GestorDeClientes {
     }
 
     public boolean adicionarCliente(int nif, String nome, int telefone, GestorDeBaseDeDados gestorBD){
+        if(nif < 1 || nif > Integer.MAX_VALUE) throw new InvalidParameterException("NIF Inválido");
+        if(nome.isEmpty() || nome.isBlank()) throw new InvalidParameterException("Nome Inválido");
+        if(telefone < 1 || telefone > Integer.MAX_VALUE) throw new InvalidParameterException("Telefone Inválido");
+        if(gestorBD == null) throw new InvalidParameterException("Gestor de Base de Dados nulo.");
         String query = String.format("REPLACE INTO cliente VALUES ('%d', '%s', '%d')", nif, nome, telefone);
-
-        try {
-            gestorBD.tryUpdateDatabase(query);
-            return true;
-        }catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        gestorBD.tryUpdateDatabase(query);
+        return true;
     }
 }
